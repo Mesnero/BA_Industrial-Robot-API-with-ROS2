e9f2d0350b2a73f8a679deffc4e3809b7f9231f8
Johannes Mesner <johannes.mesner@tum.de>
2024-10-24 02:09:02 +0200

    Tried to understand, and also improve the robco_description package. Rewritten the launch file, to be more understandable and removed unnessesary dependencies out of the package.xml file.

--- src/robco_description/launch/gazebo_launch.py
+++ src/robco_description/launch/gazebo_launch.py
@@ -4,32 +4,42 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 import os
 
-def generate_launch_description():
-    # Path to your package
-    package_name = 'robco_description'
-    world_file_name = 'my_world.sdf'
+# Constants
+PACKAGE_NAME = 'robco_description'
+WORLD_FILE_NAME = 'my_world.sdf'
+URDF_FILE_NAME = 'demo_robot.xacro'
+ROBOT_NAME = 'demo_robot'
+GAZEBO_ROS_PACKAGE_NAME = 'gazebo_ros'
+GAZEBO_LAUNCH_FILE_NAME = 'gazebo.launch.py'
 
+def generate_launch_description():
     # Find the package path
-    package_path = os.popen(f'ros2 pkg prefix {package_name}').read().strip()
+    package_path = os.popen(f'ros2 pkg prefix {PACKAGE_NAME}').read().strip()
 
     # Full path to the world file
-    world_path = os.path.join(package_path, 'share', package_name, 'worlds', world_file_name)
+    world_path = os.path.join(package_path, 'share', PACKAGE_NAME, 'worlds', WORLD_FILE_NAME)
 
     # Full path to the URDF file
-    urdf_file = os.path.join(package_path, 'share', package_name, 'urdf', 'demo_robot.xacro')
+    urdf_path = os.path.join(package_path, 'share', PACKAGE_NAME, 'urdf', URDF_FILE_NAME)
+
+    # Find the gazebo_ros package path
+    gazebo_ros_path = os.popen(f'ros2 pkg prefix {GAZEBO_ROS_PACKAGE_NAME}').read().strip()
+
+    # Full path to the gazebo launch file
+    gazebo_launch_path = os.path.join(gazebo_ros_path, 'share', GAZEBO_ROS_PACKAGE_NAME, 'launch', GAZEBO_LAUNCH_FILE_NAME)
 
     return LaunchDescription([
+        # Launch gazebo with the world file
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([os.path.join(
-                os.popen('ros2 pkg prefix gazebo_ros').read().strip(),
-                'share', 'gazebo_ros', 'launch', 'gazebo.launch.py'
-            )]),
+            PythonLaunchDescriptionSource([gazebo_launch_path]),
             launch_arguments={'world': world_path}.items()
         ),
+        # Launches Node to spawn the robot in gazebo
         Node(
-            package='gazebo_ros',
+            package=GAZEBO_ROS_PACKAGE_NAME,
             executable='spawn_entity.py',
-            arguments=['-entity', 'demo_robot', '-file', urdf_file],
-            output='screen'),
+            arguments=['-entity', ROBOT_NAME, '-file', urdf_path],
+            output='screen'
+        )
     ])
 
